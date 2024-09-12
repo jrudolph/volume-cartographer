7a18f7f62b3fd1858a17a0759ff9972d7fb3da1e
Hendrik Schilling <hendrik.schilling@posteo.de>
2024-09-12 15:41:23 +0200

    get chunk id

--- apps/src/ZarrExtract.cpp
+++ apps/src/ZarrExtract.cpp
@@ -3,8 +3,12 @@
 
 #include "z5/factory.hxx"
 #include "z5/filesystem/handle.hxx"
+#include "z5/filesystem/dataset.hxx"
+#include "z5/common.hxx"
 #include "z5/multiarray/xtensor_access.hxx"
 #include "z5/attributes.hxx"
+
+using shape = z5::types::ShapeType;
 
 std::ostream& operator<< (std::ostream& out, const std::vector<size_t> &v) {
     if ( !v.empty() ) {
@@ -16,6 +20,15 @@ std::ostream& operator<< (std::ostream& out, const std::vector<size_t> &v) {
     return out;
 }
 
+shape chunkId(const std::unique_ptr<z5::Dataset> &ds, shape coord)
+{
+    shape div = ds->chunking().blockShape();
+    shape id = coord;
+    for(int i=0;i<coord.size();i++)
+        id[i] /= div[i];
+    return id;
+}
+
 int main(int argc, char *argv[]) {
 
   assert(argc == 2);
@@ -25,5 +38,19 @@ int main(int argc, char *argv[]) {
   std::unique_ptr<z5::Dataset> ds = z5::filesystem::openDataset(ds_handle);
 
   std::cout << "ds shape " << ds->shape() << "\n";
+  std::cout << "ds shape via chunk " << ds->chunking().shape() << "\n";
+  std::cout << "chunk shape shape " << ds->chunking().blockShape() << "\n";
+
+  //read the chunk around pixel coord
+  shape coord = shape({0,2000,2000});
+
+  shape id = chunkId(ds, coord);
+
+  fs::path path;
+  ds->chunkPath(id, path);
+
+  std::cout << "id " << id << " path " << path << "\n";
+
+
   return 0;
 }
