b5cb6d1eef877c9ea29dfc0080a21511af327145
Hendrik Schilling <hendrik.schilling@posteo.de>
2024-09-23 23:28:12 +0200

    cleanup and remove any references to forked z5

--- apps/src/ZarrExtract.cpp
+++ apps/src/ZarrExtract.cpp
@@ -83,46 +83,6 @@ shape idCoord(const std::unique_ptr<z5::Dataset> &ds, shape id)
     return coord;
 }
 
-class CacheEntry
-{
-public:
-    std::vector<std::uint8_t> data;
-    z5::types::ShapeType shape;
-    std::size_t size;
-};
-
-size_t cache_hits = 0;
-size_t cache_miss = 0;
-
-//lazy for now, for 3d chunk cache this should be good enough.
-std::unordered_map<uint64_t,CacheEntry> cache;
-
-
-void putCache(z5::types::ShapeType chunkId, void* chunk, z5::types::ShapeType chunkShape, std::size_t chunkSize)
-{
-    uint64_t key = (chunkId[0]) ^ (chunkId[1]<<24) ^ (chunkId[2]<<48);
-    if (cache.find(key) != cache.end())
-        return;
-    
-    CacheEntry entry = {*static_cast<std::vector<std::uint8_t>*>(chunk), chunkShape, chunkSize};
-    cache[key] = entry;
-}
-
-void *pullCache(z5::types::ShapeType chunkId, z5::types::ShapeType& chunkShape, std::size_t& chunkSize)
-{
-    uint64_t key = (chunkId[0]) ^ (chunkId[1]<<24) ^ (chunkId[2]<<48);
-    if (cache.find(key) == cache.end()) {
-        cache_miss++;
-        return nullptr;
-    }
-    
-    cache_hits++;
-    CacheEntry &entry = cache[key];
-    chunkShape = entry.shape;
-    chunkSize = entry.size;
-    return &entry.data;
-}
-
 void timed_plane_slice(const PlaneCoords &plane, z5::Dataset *ds, size_t size, ChunkCache *cache, std::string msg)
 {
     xt::xarray<float> coords;
@@ -150,57 +110,8 @@ int main(int argc, char *argv[])
   std::cout << "ds shape via chunk " << ds->chunking().shape() << std::endl;
   std::cout << "chunk shape shape " << ds->chunking().blockShape() << std::endl;
 
-  ds->enableCaching(true, putCache, pullCache);
-  
-  
-  
-  //read the chunk around pixel coord
-  // shape coord = shape({0,2000,2000});
-
-  // shape id = chunkId(ds, coord);
-
-  // fs::path path;
-  // ds->chunkPath(id, path);
-
-//   std::cout << "id " << id << " path " << path << std::endl;
-// 
-//   int threads = static_cast<int>(std::thread::hardware_concurrency());
-// 
-//   z5::types::ShapeType offset = idCoord(ds, id);
-//   xt::xarray<uint8_t>::shape_type shape = ds->chunking().blockShape();
-//   shape[0] = 1;
-//   xt::xarray<uint8_t> array(shape);
-// 
-//   z5::multiarray::readSubarray<uint8_t>(*ds, array, offset.begin(), threads);
-// 
-//   cv::Mat m = cv::Mat(shape[1], shape[2], CV_8U, array.data());
-// 
-//   cv::imwrite("extract.tif", m);
-// 
   xt::xarray<float> coords = xt::zeros<float>({500,500,3});
   xt::xarray<uint8_t> img;
-//   
-//   cv::Vec3d slice_center = {0.5,0.5,0.5};
-//   cv::Vec3d slice_normal = {0.5,0.5,0.5};
-  
-//   for(int y=0;y<500;y++)
-//       for(int x=0;x<500;x++) {
-//           coords(y,x,0) = 500;
-//           coords(y,x,1) = y+2000;
-//           coords(y,x,2) = x+2000;
-//       }
-// 
-//   readInterpolated3D(img,ds,coords);
-//   m = cv::Mat(img.shape(0), img.shape(1), CV_8U, img.data());
-//   cv::imwrite("img.tif", m);
-// 
-// 
-//   readInterpolated3DChunked(img,ds,coords,256);
-//   
-//   
-//   m = cv::Mat(img.shape(0), img.shape(1), CV_8U, img.data());
-//   
-//   cv::imwrite("img2.tif", m);
   
   PlaneCoords gen_plane({2000,2000,2000},{0.5,0.5,0.5});
   // PlaneCoords gen_plane({2000,2000,2000},{0.0,0.0,1.0});
@@ -216,17 +127,11 @@ int main(int argc, char *argv[])
 
   ChunkCache chunk_cache(10e9);
   
-  // readInterpolated3D_a2(img,ds.get(),coords);
-  
-//   return 0;
-//   
-//   // for(int i=0;i<64;i++)
   auto start = std::chrono::high_resolution_clock::now();
   readInterpolated3D(img,ds.get(),coords, &chunk_cache);
   auto end = std::chrono::high_resolution_clock::now();
   std::cout << std::chrono::duration<double>(end-start).count() << "s cold" << std::endl;
   
-  // readInterpolated3D(img,ds.get(),coords);
   cv::Mat m = cv::Mat(img.shape(0), img.shape(1), CV_8U, img.data());
   cv::imwrite("plane.tif", m);
   
