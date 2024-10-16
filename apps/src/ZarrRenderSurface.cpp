c2120519eff19762a2dedec3e52edacf9050f634
Hendrik Schilling <hendrik.schilling@posteo.de>
2024-10-16 18:16:49 +0200

    perf baseline

--- apps/src/ZarrRenderSurface.cpp
+++ apps/src/ZarrRenderSurface.cpp
@@ -483,7 +483,15 @@ int main(int argc, char *argv[])
     
     QuadSurface *surf = surf_raw;
     SurfacePointer *poi = surf->pointer();
-    surf->move(poi, {-10200,-13200,0});
+    //took 0.0722211 s
+    //gen 45 processing 0 fringe cands (total succ/fail 1511/0 fringe: 1 skipped: 6 failures: 0
+    // -> total succ/fail 1511/0 fringe: 0 skipped: 6 failures: 0
+    // surf->move(poi, {-10200,-13200,0});
+
+    //took 5.13091 s
+    //gen 57 processing 0 fringe cands (total succ/fail 2397/5 fringe: 1 skipped: 0 failures: 0
+    // -> total succ/fail 2397/5 fringe: 0 skipped: 0 failures: 0
+    surf->move(poi, {-78.09/0.159367,-460.323/0.476525,0});
     {
         MeasureLife timer("build local mesh ...");
         surf = regularized_local_quad(surf_raw, poi, w/mesh_step/output_scale, h/mesh_step/output_scale, search_step, mesh_step);
