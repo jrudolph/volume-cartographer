5c2077868b0321a9a146d38403a81734d8159815
Hendrik Schilling <hendrik.schilling@posteo.de>
2024-11-25 13:30:49 +0100

    Revert "split surface code"
    This reverts commit bce2bdb551bab4dbb34d030b69311784224092cd.

---
+++ apps/src/vc_grow_seg_from_segments.cpp
@@ -0,0 +1,171 @@
+#include <nlohmann/json.hpp>
+
+#include <xtensor/xarray.hpp>
+#include <xtensor/xaxis_slice_iterator.hpp>
+#include <xtensor/xio.hpp>
+#include <xtensor/xbuilder.hpp>
+#include <xtensor/xview.hpp>
+
+#include "z5/factory.hxx"
+#include "z5/filesystem/handle.hxx"
+#include "z5/filesystem/dataset.hxx"
+#include "z5/common.hxx"
+#include "z5/multiarray/xtensor_access.hxx"
+#include "z5/attributes.hxx"
+
+#include <opencv2/highgui.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+
+#include "vc/core/util/Slicing.hpp"
+#include "vc/core/util/Surface.hpp"
+#include "vc/core/io/PointSetIO.hpp"
+
+#include <unordered_map>
+#include <filesystem>
+#include <omp.h>
+
+#include "vc/core/types/ChunkedTensor.hpp"
+
+using shape = z5::types::ShapeType;
+using namespace xt::placeholders;
+namespace fs = std::filesystem;
+
+using json = nlohmann::json;
+
+std::ostream& operator<< (std::ostream& out, const xt::svector<size_t> &v) {
+    if ( !v.empty() ) {
+        out << '[';
+        for(auto &v : v)
+            out << v << ",";
+        out << "\b]"; // use ANSI backspace character '\b' to overwrite final ", "
+    }
+    return out;
+}
+
+class MeasureLife
+{
+public:
+    MeasureLife(std::string msg)
+    {
+        std::cout << msg << std::flush;
+        start = std::chrono::high_resolution_clock::now();
+    }
+    ~MeasureLife()
+    {
+        auto end = std::chrono::high_resolution_clock::now();
+        std::cout << " took " << std::chrono::duration<double>(end-start).count() << " s" << std::endl;
+    }
+private:
+    std::chrono::time_point<std::chrono::high_resolution_clock> start;
+};
+
+std::string time_str()
+{
+    using namespace std::chrono;
+
+    // get current time
+    auto now = system_clock::now();
+
+    // get number of milliseconds for the current second
+    // (remainder after division into seconds)
+    auto ms = duration_cast<milliseconds>(now.time_since_epoch()) % 1000;
+
+    // convert to std::time_t in order to convert to std::tm (broken time)
+    auto timer = system_clock::to_time_t(now);
+
+    // convert to broken time
+    std::tm bt = *std::localtime(&timer);
+
+    std::ostringstream oss;
+
+    oss << std::put_time(&bt, "%Y%m%d%H%M%S"); // HH:MM:SS
+    oss << std::setfill('0') << std::setw(3) << ms.count();
+
+    return oss.str();
+}
+
+template <typename T, typename I>
+float get_val(I &interp, cv::Vec3d l) {
+    T v;
+    interp.Evaluate(l[2], l[1], l[0], &v);
+    return v;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 6) {
+        std::cout << "usage: " << argv[0] << " <zarr-volume> <src-dir> <tgt-dir> <json-params> <src-segment>" << std::endl;
+        return EXIT_SUCCESS;
+    }
+
+    fs::path vol_path = argv[1];
+    fs::path src_dir = argv[2];
+    fs::path tgt_dir = argv[3];
+    fs::path params_path = argv[4];
+    fs::path src_path = argv[5];
+    while (src_path.filename().empty())
+        src_path = src_path.parent_path();
+
+    std::ifstream params_f(params_path);
+    json params = json::parse(params_f);
+
+    z5::filesystem::handle::Group group(vol_path, z5::FileMode::FileMode::r);
+    z5::filesystem::handle::Dataset ds_handle(group, "0", json::parse(std::ifstream(vol_path/"0/.zarray")).value<>("dimension_separator","."));
+    std::unique_ptr<z5::Dataset> ds = z5::filesystem::openDataset(ds_handle);
+
+    std::cout << "zarr dataset size for scale group 0 " << ds->shape() << std::endl;
+    std::cout << "chunk shape shape " << ds->chunking().blockShape() << std::endl;
+
+    std::string name_prefix = "auto_grown_";
+    std::vector<SurfaceMeta*> surfaces;
+
+    fs::path meta_fn = src_path / "meta.json";
+    std::ifstream meta_f(meta_fn);
+    json meta = json::parse(meta_f);
+    SurfaceMeta *src = new SurfaceMeta(src_path, meta);
+    src->readOverlapping();
+
+    for (const auto& entry : fs::directory_iterator(src_dir))
+        if (fs::is_directory(entry)) {
+            std::string name = entry.path().filename();
+            if (name.compare(0, name_prefix.size(), name_prefix))
+                continue;
+
+            fs::path meta_fn = entry.path() / "meta.json";
+            if (!fs::exists(meta_fn))
+                continue;
+
+            std::ifstream meta_f(meta_fn);
+            json meta = json::parse(meta_f);
+
+            if (!meta.count("bbox"))
+                continue;
+
+            if (meta.value("format","NONE") != "tifxyz")
+                continue;
+
+            SurfaceMeta *sm;
+            if (entry.path().filename() == src->name())
+                sm = src;
+            else {
+                sm = new SurfaceMeta(entry.path(), meta);
+                sm->readOverlapping();
+            }
+
+            surfaces.push_back(sm);
+        }
+
+    QuadSurface *surf = grow_surf_from_surfs(src, surfaces, 10.0);
+
+    if (!surf)
+        return EXIT_SUCCESS;
+
+    (*surf->meta)["source"] = "vc_grow_seg_from_segments";
+    // std::string uuid = "testing_auto_surf_trace" + std::string("_opt_int_64%6_") + time_str();
+    std::string uuid = "testing_auto_surf_trace";;
+    fs::path seg_dir = tgt_dir / uuid;
+    surf->save(seg_dir, uuid);
+
+    return EXIT_SUCCESS;
+}
