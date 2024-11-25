f6a4b04aa5a6c5960ce75aa01e09cd959a31ecc3
Hendrik Schilling <hendrik.schilling@posteo.de>
2024-11-25 13:43:37 +0100

    Revert "Merge branch 'dev-zarr' into dev-surfacemodeling"
    This reverts commit 3101c5b2bc1fc509d0b723ef25eafb4126151e31, reversing
    changes made to 5c2077868b0321a9a146d38403a81734d8159815.

--- core/include/vc/core/util/Surface.hpp
+++ core/include/vc/core/util/Surface.hpp
@@ -226,3 +226,5 @@ bool contains(SurfaceMeta &a, const std::vector<cv::Vec3f> &locs);
 void find_intersect_segments(std::vector<std::vector<cv::Vec3f>> &seg_vol, std::vector<std::vector<cv::Vec2f>> &seg_grid, const cv::Mat_<cv::Vec3f> &points, PlaneSurface *plane, const cv::Rect &plane_roi, float step, int min_tries = 10);
 
 float min_loc(const cv::Mat_<cv::Vec3f> &points, cv::Vec2f &loc, cv::Vec3f &out, const std::vector<cv::Vec3f> &tgts, const std::vector<float> &tds, PlaneSurface *plane, float init_step = 16.0, float min_step = 0.125);
+
+QuadSurface *grow_surf_from_surfs(SurfaceMeta *seed, const std::vector<SurfaceMeta*> &surfs_v, float step);
